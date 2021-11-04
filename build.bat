6e2ec9f905f507553da256c79d924903b8192b15
Andrew Maney <67339620+MEMESCOEP@users.noreply.github.com>
2021-11-04 10:32:47 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 cls
 echo AUSL Build Script
 set pypth=%1
-set pyexe=%pypth%\python.exe -m nuitka --standalone --onefile --follow-imports .\AUSL_latest\AUSL.py
+set pyexe=%pypth%\python.exe -m nuitka --standalone --onefile --follow-imports %pypth%\AUSL_latest\AUSL.py
 echo PYPATH: "%pyexe%"
 %pyexe%
 pause
