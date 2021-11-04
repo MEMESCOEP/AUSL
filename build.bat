65bcd74215fedcb47ee631a9217e3ba718260656
Andrew Maney <67339620+MEMESCOEP@users.noreply.github.com>
2021-11-04 10:05:13 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
-echo "\nAUSL Build Script\n"
-python3 -m nuitka --standalone --onefile --follow-imports AUSL.py
+@echo off
+echo "AUSL Build Script"
+SET pypth = %1
+set pyexe = %pypth% + python.exe
+start %pyexe% -m nuitka --standalone --onefile --follow-imports AUSL.py
